a008fd0a73f74813255448f3efa9e76a445a647e
Jasiek Kostecki <bialykostek@gmail.com>
2023-04-06 19:31:36 +0200

    server task distributions

--- generate.py
+++ generate.py
@@ -8,10 +8,15 @@ W = 1
 M = []
 clients = []
 started = False
+tasks = []
+batch_size = 10
+x_size = 100
+task_marker = 0
 
 def generate_matrices():
     global W
     global M
+    global tasks
 
     start_time = time.time()
 
@@ -19,8 +24,6 @@ def generate_matrices():
     x_max = 10
     a_min = -10
     a_max = 10
-
-    x_size = 1000
 
     A = np.random.rand(x_size, x_size)*(a_max-a_min)+a_min
     X_old = np.random.rand(x_size, 1)*(x_max-x_min)+x_min
@@ -35,8 +38,8 @@ def generate_matrices():
         for k in range(x_size):
             tmp[k][i] = B[k][0]
         M.append(tmp)
+        tasks.append([0, 0, 0])
 
-    
     for ind, matrix in enumerate(M):
         with open('data/data' + str(ind) + '.txt','w') as f:
             np.savetxt(f, matrix, fmt='%g', delimiter=' ')
@@ -45,7 +48,7 @@ def generate_matrices():
     
     for ind, matrix in enumerate(M):
         h5f = h5py.File('data/data'+str(ind)+'.h5', 'w')
-        h5f.create_dataset('dataset_1', data=matrix)
+        h5f.create_dataset('dataset', data=matrix)
         print(str(ind) + "/" + str(x_size))  
     
     print("generated in " + str(time.time() - start_time))
@@ -58,10 +61,13 @@ def generate_matrices():
 
 class SolverServer(WebSocket):
     def handle(self):
+        global task_marker
         '''
         0 - login gpu
         1 - generate data
         2 - start computing
+        3 - task list for gpu
+        4 - solution receive
         '''
 
         data = json.loads(self.data)
@@ -72,13 +78,52 @@ class SolverServer(WebSocket):
         if data['type'] == 1:
             generate_matrices()
 
+        if data['type'] == 2:
+            for client in clients:
+                print("XD")
+                to_solve = []
+                safety = 0
+                for i in range(batch_size):
+                    while True:
+                        print("XDDDD")
+                        '''
+                        task: [state, solver, result]
+                        task state:
+                        0 - not send to solver
+                        1 - waiting for result
+                        2 - solved
+                        '''
+                        if(tasks[task_marker][0] == 0):
+                            to_solve.append(task_marker)
+                            print(to_solve)
+                            tasks[task_marker][0] = 1
+                            task_marker += 1
+                            if task_marker == x_size:
+                                task_marker = 0
+                            break
+                            
+                        safety += 1    
+                        if safety > x_size:
+                            break
+                print(json.dumps({
+                    'type': 3,
+                    'to_solve': to_solve
+                }))
+                client.send_message(json.dumps({
+                    'type': 3,
+                    'to_solve': to_solve
+                }))
+
+        if data['type'] == 4:
+            print(data)
+
     def connected(self):
         print("connected")
 
     def handle_close(self):
         clients.remove(self)
 
-generate_matrices()
+#generate_matrices()
 server = WebSocketServer('', 1111, SolverServer)
 server.serve_forever()
 
--- test_client.py
+++ test_client.py
@@ -2,9 +2,18 @@ import websocket
 import _thread
 import time
 import rel
+import numpy as np
+import threading
+import json
+import h5py
+
+to_solve = []
 
 def on_message(ws, message):
-    print(message)
+    global to_solve
+    data = json.loads(message)
+    if data['type'] == 3:
+        to_solve = data['to_solve'].copy()
 
 def on_error(ws, error):
     print(error)
@@ -13,7 +22,9 @@ def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
 def on_open(ws):
-    print("Opened connection")
+    ws.send(json.dumps({'type': 0}))
+    print("Connected")
+
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
@@ -22,7 +33,21 @@ if __name__ == "__main__":
                               on_message=on_message,
                               on_error=on_error,
                               on_close=on_close)
+    wst = threading.Thread(target=ws.run_forever)
+    wst.daemon = True
+    wst.start()
 
-    ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
-    rel.signal(2, rel.abort)  # Keyboard Interrupt
-    rel.dispatch()
+    while True:
+        print(to_solve)
+        if len(to_solve) > 0:
+            for task in to_solve:
+                hf = h5py.File('data/data' + str(task) + '.h5', 'r')
+                matrix = np.array(hf["dataset"][:])
+
+                print(matrix)
+
+                ws.send(json.dumps({'type': 4, 'task': task, 'solution': np.linalg.det(matrix)}))
+                to_solve.remove(task)
+
+        else:
+            time.sleep(0.1)
