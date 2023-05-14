b6fc74859b364ec762d20430d50a7c14d6bc7754
szymonberdzik <szymon.berdzik@softnauts.com>
2023-05-14 17:25:33 +0200

    fix

--- server/src/workers.py
+++ server/src/workers.py
@@ -43,15 +43,13 @@ def check_if_worker_exists_by_status_port_and_ip(port: int, ip: str):
     for  key,worker in enumerate(workers):
         if worker['status_port'] == port and worker['ip'] == ip:
             return key;
-        else:
-            return None
+    return None
         
 def check_if_worker_exists_by_app_port_and_ip(port: int, ip: str):
     for  key,worker in enumerate(workers):
         if worker['app_port'] == port and worker['ip'] == ip:
             return key;
-        else:
-            return None
+    return None
 
 def get_all_online_workers() -> dict:
         return {key: worker for key, worker in workers if worker["status_port"] == "online"}
